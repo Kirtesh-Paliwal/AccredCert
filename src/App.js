f6521e63c77cc5a41cb7a7b722340f6e0127bb2c
Your Name <kirteshpaliwal346@gmail.com>
2025-03-29 15:01:31 +0530

    Fix App export issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var react_router_dom_1 = require("react-router-dom");
-require("./App.css");
-var Home_1 = require("./pages/Home");
-var React = require("react");
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home"; // Ensure this is a default export
+
 const App = () => {
-    return (React.createElement(react_router_dom_1.BrowserRouter, null,
-        React.createElement(react_1.Suspense, { fallback: React.createElement("div", { className: "flex justify-center items-center h-screen" },
-                React.createElement("div", { className: "animate-spin rounded-full h-16 w-16 border-t-4 border-yellow-400 border-opacity-75" })) },
-            React.createElement(react_router_dom_1.Routes, null,
-                React.createElement(react_router_dom_1.Route, { path: "/", element: React.createElement(Home_1.default, null) })))));
+  return (
+    <BrowserRouter>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-yellow-400 border-opacity-75"></div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 };
+
 export default App;
